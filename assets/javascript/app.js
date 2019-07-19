aad535139631926c0a47a93bda60c5cee51def83
Evan Desktop <evanjcleary@gmail.com>
2019-07-19 18:38:21 -0400

    added psuedocode for the game

---
+++ assets/javascript/app.js
@@ -0,0 +1,38 @@
+//Psuedo Code//
+
+
+// Create Characters
+    //These characters should be objects with: name, HP, attacks functions
+var Character1;
+var Character2;
+var Character3;
+var Character4;
+var Character5;
+
+//Create a Play Selection Container
+
+// Create a box in HTML for each character including an image / sprite
+
+//Create a Battlefield container 
+    // Create an Attacker section
+    // Create a Defender section
+
+// When player is selected move that character into the attacker box
+
+// User picks the enemy then move that into the defender box
+
+// Create an attack button that will minus the hitpoints of the Defender's HP
+
+// Put a tracker on the Defender's HP to run an endgame function when Defender's HP reaches 0
+
+// Create a function where the enemy will return an attack after the user fires their attack
+
+// Put a tracker on the Attacker's HP to run an endgame function when Attacker HP reaches 0
+
+// Once Enemy is defeated load up the next enemy 
+
+// Repeat the same processes as above
+
+// Once all enemies are defeated run a winGame scenario
+
+// Otherwise if attacker loses run a loseGame scenario
